cc3efc4aa5012330072d69acb26c73e1604a1d5c
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-05-18 10:00:28 +0100

    CI added to package.json

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -7,75 +7,80 @@
 
 // export default Chart
 
+import React, { PureComponent } from 'react'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts'
 
-import React, { PureComponent } from 'react';
-import { LineChart, Line, XAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-import "./chart.css"
+import './chart.css'
 
 export default function Chart() {
-
-
   const data = [
     {
       name: 'Jan',
-      "Acitive User": 1902,
+      'Acitive User': 1902,
     },
     {
       name: 'Feb',
-      "Acitive User": 2990,
+      'Acitive User': 2990,
     },
     {
       name: 'Mar',
-      "Active User": 4830,
+      'Active User': 4830,
     },
     {
       name: 'April',
-      "Active User": 5020,
+      'Active User': 5020,
     },
     {
       name: 'May',
-      "Active User": 3200,
+      'Active User': 3200,
     },
     {
       name: 'June',
-      "Active User": 5100,
+      'Active User': 5100,
     },
     {
       name: 'July',
-      "Active User": 3639,
+      'Active User': 3639,
     },
     {
       name: 'Aug',
-      "Active User": 5979,
+      'Active User': 5979,
     },
     {
       name: 'Sept',
-      "Active User": 6001,
+      'Active User': 6001,
     },
     {
       name: 'Oct',
-      "Active User": 7800,
+      'Active User': 7800,
     },
     {
       name: 'Nov',
-      "Active User": 7549,
+      'Active User': 7549,
     },
     {
       name: 'Dec',
-      "Active User": 8038,
+      'Active User': 8038,
     },
-  ];
+  ]
 
   return (
     <div className='chart'>
-      <h3 className="chartTitle">Sales Analytics</h3>
-      <ResponsiveContainer width={"100%"} aspect={4 / 1}>
+      <h3 className='chartTitle'>Sales Analytics</h3>
+      <ResponsiveContainer width={'100%'} aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey={"name"} stroke={"#5550bd"} />
-          <Line type={"monotone"} dataKey={"Active User"} stroke={"#5550bd"} />
+          <XAxis dataKey={'name'} stroke={'#5550bd'} />
+          <Line type={'monotone'} dataKey={'Active User'} stroke={'#5550bd'} />
           <Tooltip />
-          <CartesianGrid stroke={"#e0dfdf"} strokeDasharray="5 5" />
+          <CartesianGrid stroke={'#e0dfdf'} strokeDasharray='5 5' />
           <Legend />
         </LineChart>
       </ResponsiveContainer>
