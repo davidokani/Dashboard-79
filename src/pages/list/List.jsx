9b5315b2b29ba2a8d417754261790dfc3991a1ec
David Okani <itsdavidokani@gmail.com>
2022-05-18 08:55:51 +0100

    Done check up work

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+// import React from 'react'
 
-const List = () => {
-  return <div>List</div>
+// const List = () => {
+//   return <div>List</div>
+// }
+
+// export default List
+
+import "./list.css"
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+export default function List() {
+  return (
+    <div className='list'>List</div>
+  )
 }
-
-export default List
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 const Login = () => {
   return <div>
-    <form action="">
-      <h2>Hi Stranger, Welcome back.</h2>
-      <input type="text" placeholder='Email' />
-      <input type="password" placeholder='Password' maxLength={12} />
-      <button>Sign In</button>
-    </form>
+    <div className='container'>
+      <form action="">
+        <h2>Hi Stranger, Welcome back.</h2>
+        <label htmlFor="email">Email</label>
+        <input id='email' type="text" placeholder='Email' />
+        <label htmlFor="password">Password</label>
+        <input id='password' type="password" placeholder='Password' maxLength={12} />
+        <button>Sign In</button>
+      </form>
+    </div>
   </div>
 }
 
